4907ba55881922fd5a24b91b6f13589dd053a569
Sükrü <baerlim.com@gmail.com>
2025-10-31 22:15:02 +0100

    moved User.js into models folder and fixed import path

--- baerlim-backend/authRoutes.js
+++ baerlim-backend/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
-import User from "../models/User.js";
+import User from "./models/User.js";
 
 const router = express.Router();
 
